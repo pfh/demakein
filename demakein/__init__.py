aceef630b2ad61aafd61f9da286620bc6fad96d3
Paul Harrison <pfh@logarithmic.net>
2024-04-27 16:45:45 +1000

    Include a long description for the Python Package Index.

--- demakein/__init__.py
+++ demakein/__init__.py
@@ -1,4 +1,4 @@
-VERSION = '0.17'
+VERSION = '0.18'
 
 from .design_flute import Design_pflute, Design_folk_flute
 from .make_flute import Make_flute
--- setup.py
+++ setup.py
@@ -9,9 +9,37 @@ from setuptools import setup
 with open(os.path.normpath(os.path.join(__file__,'..','demakein','__init__.py')),'rU') as f:
     exec f.readline()
 
+long_description = """
+
+Demakein is a set of Python tools for designing and making woodwind 
+instruments.
+
+This generally consists of two stages:
+
+- The "design" stage is a numerical optimization that chooses the bore shape and the finger hole placement, size, and depth necessary for the instrument to produce the correct notes for a given set of fingerings.
+
+- The "make" stage takes a design and turns it into a 3D object, then then cuts the object into pieces that can be CNC-milled or 3D-printed.
+
+Demakein can either be used via the command "demakein" or as a library in Python. Demakein has been designed to be extensible, and I hope you will find it relatively easy to write code to create your own novel instruments. You can either create subclasses of existing classes in order to tweak a few parameters, or create wholly new classes using existing examples as a template.
+
+See the README for detailed install instructions:
+
+https://github.com/pfh/demakein
+
+Home page:
+
+http://www.logarithmic.net/pfh/design
+
+Author:
+
+Paul Harrison, pfh@logarithmic.net or paul.francis.harrison@gmail.com
+
+"""
+
 setup(name='demakein',
       version=VERSION,
       description='Design woodwind instruments and make them with a 3D printer or CNC mill.',
+      long_description=long_description,
       
       packages = [
           'demakein', 
